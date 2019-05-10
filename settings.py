41f7adb1bd593e7b4d5d2ea153d3f79e56daad75
ChenKun1997 <2310353398@qq.com>
2019-05-10 17:05:16 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,25 @@
+class Settings():
+    """存储《外星人入侵》的所有设置的类"""
+
+    def __init__(self):
+        """初始化游戏的设置"""
+        #屏幕设置
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230,230,200)
+
+        #飞船的设置
+        self.ship_speed_factor = 1.5
+        self.ship_limit = 3
+
+        #子弹设置
+        self.bullet_speed_factor = 3
+        self.bullet_width = 2
+        self.bullet_height = 15
+        self.bullet_color = 60,60,60
+        self.bullets_allowed = 3
+
+        #外星人设置
+        self.alien_speed_factor = 0.3
+        self.fleet_direction = -1
+        self.fleet_drop_speed = 30
